d1bac481bd7116fde9a7dad43cc3316b58a9ac96
Akshay Nair <phenax5@gmail.com>
2025-06-29 21:36:37 +0530

    Add swipe left/right to go to next/prev item

--- src/assets/javascripts/app.js
+++ src/assets/javascripts/app.js
@@ -47,6 +47,31 @@ Vue.directive('focus', {
   }
 })
 
+Vue.directive('swipe-items', {
+  inserted(el, _, vnode) {
+    if (!window.Hammer) return;
+    delete Hammer.defaults.cssProps.userSelect; // Allow selection
+    const hammer = new Hammer.Manager(el, {
+      domEvents: true,
+      touchAction: 'auto',
+      inputClass: Hammer.TouchInput,
+      recognizers: [
+        [Hammer.Swipe, {
+          direction: Hammer.DIRECTION_HORIZONTAL,
+          threshold: 50,
+          velocity: 1,
+        }]
+      ]
+    });
+    hammer.on('swipeleft', (_) => {
+      vnode.context.navigateToItem(+1)
+    })
+    hammer.on('swiperight', (_) => {
+      vnode.context.navigateToItem(-1)
+    })
+  }
+})
+
 Vue.component('drag', {
   props: ['width'],
   template: '<div class="drag"></div>',
